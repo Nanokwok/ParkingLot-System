2cdb06925ae3b592092cd15edc1829c775506a37
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-03-27 19:07:36 +0700

    Handle the MongoDB connection

---
+++ lib/mongodb.ts
@@ -0,0 +1,37 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+/** 
+ * Cached connection for MongoDB.
+ */
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseCache = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect(): Promise<Mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
